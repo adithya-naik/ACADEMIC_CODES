43517a56ded8c6d96e75d16e0a8ee0d7ccc7c2c8
adithya-naik <jatothadithyanaik@gmail.com>
2024-04-02 00:20:17 +0530

    added

---
+++ JAVA/SEM III OOPJ/toString.java
@@ -0,0 +1,7 @@
+public class toString {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("hey ");
+        sb.append("guys");
+        System.out.println(sb);
+    }
+}
